55f00c81a2ef33544919292fb75e8911575e2f9d
Sude Kargı <sude.kargi2408@gmail.com>
2023-08-10 17:02:55 +0300

    First commit

---
+++ movie_app/lib/home_screen.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+        flexibleSpace: ClipPath(
+          clipper: _CustomClipper(),
+          child: Container(
+            height: 150,
+            width: MediaQuery.of(context).size.width,
+            color: const Color.fromARGB(255, 3, 26, 59),
+            child: Center(
+              child: Text(
+                'Popular Movies',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall!
+                    .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+      ),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 0.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 0),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, childAspectRatio: 0.60),
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return MovieCard();
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    double height = size.height;
+    double width = size.width;
+
+    var path = Path();
+
+    path.lineTo(0, height - 50);
+    path.quadraticBezierTo(width / 2, height, width, height - 50);
+    path.lineTo(width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
+
+class MovieCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Image.network(
+              'https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 60,
+          left: 0,
+          right: 0,
+          child: Container(
+            child: Text(
+              'movie title',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+void main() => runApp(MaterialApp(
+      home: HomeScreen(),
+    ));
